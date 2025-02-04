7fd8d4bb0c6e3747f72cae9dddd0a1e00d8a02a7
BarbarianBunny <carury@gmail.com>
2025-02-03 22:19:43 -0800

    create tests for normalizeURL

---
+++ normalize_url_test.go
@@ -0,0 +1,78 @@
+package main
+
+import "testing"
+
+func TestNormalizeURL(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputURL  string
+		expected  string
+		expectErr bool
+	}{
+		{
+			name:      "remove https and slash",
+			inputURL:  "https://blog.boot.dev/path/",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "remove https",
+			inputURL:  "https://blog.boot.dev/path",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "remove http and slash",
+			inputURL:  "http://blog.boot.dev/path/",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "remove http",
+			inputURL:  "http://blog.boot.dev/path",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "remove slash",
+			inputURL:  "blog.boot.dev/path/",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "leave unchanged",
+			inputURL:  "blog.boot.dev/path",
+			expected:  "blog.boot.dev/path",
+			expectErr: false,
+		}, {
+			name:      "error no period",
+			inputURL:  "dev/path",
+			expected:  "dev/path",
+			expectErr: true,
+		}, {
+			name:      "error no domain",
+			inputURL:  "http://",
+			expected:  "http://",
+			expectErr: true,
+		},
+		// {
+		// 	name: "",
+		// 	inputURL: "",
+		// 	expected: "",
+		// 	expectErr: false,
+		// },
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := normalizeURL(tc.inputURL)
+			if err == nil && tc.expectErr {
+				t.Errorf("Test %v - '%s' FAIL: expected error: nil", i, tc.name)
+				return
+			}
+			if err != nil && !tc.expectErr {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+			if actual != tc.expected {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+				return
+			}
+		})
+	}
+}
