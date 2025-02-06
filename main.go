f8b35d338477fe44c899b54c0b60f0ed80e699fb
BarbarianBunny <carury@gmail.com>
2025-02-05 21:51:33 -0800

    add accepting 1 link as argument in main

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	lenArgs := len(os.Args)
+	if lenArgs == 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if lenArgs > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := os.Args[1]
+	fmt.Printf("starting crawl of: %v\n", baseURL)
+
 }
